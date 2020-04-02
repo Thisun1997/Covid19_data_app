998194ba2ff25982292618ab8f0b98fe95c605d7
Thisun1997 <47599759+Thisun1997@users.noreply.github.com>
2020-04-02 22:57:43 +0530

    open image added

--- lib/launch_page.dart
+++ lib/launch_page.dart
@@ -9,6 +9,10 @@ class LaunchWidget extends StatelessWidget {
         child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(bottom:40.0),
+                child: Image.network("https://www.hpb.health.gov.lk/media/banner/banner-five/5.png",scale: 2.0),
+              ),
               Text('Covid-19 Data',textScaleFactor: 3.0, style: TextStyle(color: Colors.white), ),
               Container(
                 margin: EdgeInsets.only(top:80.0),
